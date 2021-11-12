760a588446142e9e64078357190684cde47c5cbd
IvanLpCz <ivanlopezcozar@gmail.com>
2021-11-13 00:41:47 +0100

    Bala
    Arreglada

--- GameJam4/Assets/Scripts/bullets/bullet.cs
+++ GameJam4/Assets/Scripts/bullets/bullet.cs
@@ -10,27 +10,22 @@ namespace bullets
         Rigidbody rb;
         public float bulletForce = 20f;
         public Transform look;
+        private bool hasShooted;
 
-        private void Start()
+        private void Awake()
         {
             look = GameObject.Find("lookat").GetComponent<Transform>();
             rb = GetComponent<Rigidbody>();
+        }
+        private void OnEnable()
+        {
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
-
             rb.AddForce(look.forward * bulletForce, ForceMode.Impulse);
         }
-        private void FixedUpdate()
-        {
-
-            //rb.velocity = look.forward * bulletForce;
-            
-        }
-
         private void OnTriggerEnter(Collider collision)
         {
             gameObject.SetActive(false);
-
         }
     }
 }
