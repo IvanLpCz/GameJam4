63fb64b80b8b4ef1df927578649d4a7db6060c46
IvanLpCz <ivanlopezcozar@gmail.com>
2021-11-12 21:28:25 +0100

    Modelo y Bala
    Modelo añadido
    Bala avanzada

--- GameJam4/Assets/Scripts/bullets/bullet.cs
+++ GameJam4/Assets/Scripts/bullets/bullet.cs
@@ -14,18 +14,23 @@ namespace bullets
         private void Start()
         {
             look = GameObject.Find("lookat").GetComponent<Transform>();
+            rb = GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
 
+            rb.AddForce(look.forward * bulletForce, ForceMode.Impulse);
         }
         private void FixedUpdate()
         {
-            rb = GetComponent<Rigidbody>();
-            rb.velocity = look.forward * bulletForce;
+
+            //rb.velocity = look.forward * bulletForce;
             
         }
 
         private void OnTriggerEnter(Collider collision)
         {
             gameObject.SetActive(false);
+
         }
     }
 }
