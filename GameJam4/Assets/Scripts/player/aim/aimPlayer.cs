f1db7796cc432571a877a4c4b9e4db1cf5389257
IvanLpCz <ivanlopezcozar@gmail.com>
2021-11-13 11:09:07 +0100

    bullet, player update

--- GameJam4/Assets/Scripts/player/aim/aimPlayer.cs
+++ GameJam4/Assets/Scripts/player/aim/aimPlayer.cs
@@ -9,28 +9,39 @@ namespace player
     public class aimPlayer : MonoBehaviour
     {
         public Camera cam;
-        private Rigidbody rb;
-        pool Pool;
         private Vector3 mousePos;
+        public Transform bulletPosition;
+        public float cd = 0.2f;
+        private float lastShoot = 1;
         private void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-        }
-        private void Update()
-        {
-           mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            if (Input.GetKey("Fire1"))
-            {
-                shoot();
-            }
-        }
-        private void FixedUpdate()
         {
 
         }
-        private void shoot()
+        private void Update()
         {
-            
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 newMousePos = new Vector3(mousePos.x, 0f, mousePos.z);
+
+            Vector3 direction = newMousePos - transform.position;
+            Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.back);
+            transform.rotation = newRotation;
+            if (Input.GetKeyDown(KeyCode.Mouse0) && lastShoot > cd)
+            {
+                StartCoroutine(shoot());
+            }
+            lastShoot = Time.deltaTime + lastShoot;
+        }
+        IEnumerator shoot()
+        {
+            GameObject bullet = pooling.instance.GetPooledObject();
+
+            if (bullet != null)
+            {             
+                bullet.transform.position = bulletPosition.position;
+                bullet.SetActive(true);
+                lastShoot = 0f;
+            }
+            yield return null;
         }
 
     }
